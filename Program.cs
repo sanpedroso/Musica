650f8fd7239fbaf24a055f0c09a5cf21ad11104b
sanpedroso <sandropedroso@icloud.com>
2022-09-03 22:01:30 -0300

    Adicionar soluçao do exercicio

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Oh Happy Day - Edwin Hawkins");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("Oh♂ happy day");
+        Console.WriteLine("Oh happy day");
+        Console.WriteLine("When Jesus washed");
+        Console.WriteLine("Oh when He washed");
+        Console.WriteLine("Mmm, when He washed");
+        Console.WriteLine("All my sins away");
+        Console.WriteLine("Oh happy day");
+        Console.ResetColor();
+        Console.WriteLine();
+
+   Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("Oh happy day (oh happy day)");
+        Console.WriteLine("Oh happy day (oh happy day)");
+        Console.WriteLine("When Jesus washed (when Jesus washed)");
+        Console.WriteLine("Oh when He washe  (when Jesus washed)");
+        Console.WriteLine("Mmm, when He washed  (mmm, when He washed)");
+        Console.WriteLine("All my sins away (oh happy day)");
+        Console.ResetColor();
+        Console.WriteLine();
+    
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("He taught me how to watch");
+        Console.WriteLine("Watch and pray");
+        Console.WriteLine("Watch and pray (whatch and pray)");
+        Console.WriteLine("And live rejoicing every day");
+        Console.WriteLine("Every day (every day)");
+        Console.ResetColor();
+        Console.WriteLine();
+
+   Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("Oh happy day (oh happy day)");
+        Console.WriteLine("Oh happy day (oh happy day)");
+        Console.WriteLine("When Jesus washed (when Jesus washed)");
+        Console.WriteLine("Oh when He washed (when Jesus washed)");
+        Console.WriteLine("Mmm, when He washed (mmm, when He washed)");
+        Console.WriteLine("All my sins away (oh happy day)");
+        Console.ResetColor();
+        Console.WriteLine();
+    
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("It was a happy day (oh happy day)");
+        Console.ResetColor();
+        Console.WriteLine();
+
